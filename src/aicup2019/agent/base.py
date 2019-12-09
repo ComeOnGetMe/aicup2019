0d5a29aa9bb38863fc09a8807ed2e8a3229c0b9f
ComeOnGetMe <yzp451312410@gmail.com>
2019-12-09 00:08:03 -0800

    comments

--- src/aicup2019/agent/base.py
+++ src/aicup2019/agent/base.py
@@ -8,28 +8,32 @@ class BaseAgent(object):
         self.enemy = None
 
     def get_action(self, unit, game, debug):
-        # game parsing
+        # parsing
         env.parse_frame(unit, game)
 
-        # decision making:
+        # decision making
         weapon = env.nearest_weapon
         enemy = env.nearest_enemy
         if env.ego.weapon is None and weapon is not None:
             destination = weapon
         self.destination = destination
         self.enemy = enemy
-        # calculating:
-        action = self.calculate(destination, enemy)
+
+        # micro
+        action = self.micro(destination, enemy)
+
         # logging
         self.log(debug, action)
         return action
 
-    def calculate(self, dest, enemy):
+    def micro(self, dest, enemy):
+        # velocity
         ego_pos = env.ego.position
         target_pos = dest.position
         vel = target_pos.x - ego_pos.x
         vel = [-30., 30.][vel > 0]
 
+        # jump
         ego_tile = env.ego.tile_pos
         jump = target_pos.y > ego_pos.y
         if target_pos.x > ego_pos.x and \
@@ -38,19 +42,27 @@ class BaseAgent(object):
         if target_pos.x < ego_pos.x and \
                 env.tiles[ego_tile[0] - 1][ego_tile[1]] == model.Tile.WALL:
             jump = True
+        # TODO: check if above platform
+        jump_down = not jump
 
+        # attack
+        # TODO: check wall in the front
+        shoot = True
+        reload = False  # TODO
+        # TODO: prediction
         aim = model.Vec2Double(0, 0)
         if enemy is not None:
             aim = model.Vec2Double(
                 enemy.position.x - ego_pos.x,
                 enemy.position.y - ego_pos.y)
+
         return model.UnitAction(
             velocity=vel,
             jump=jump,
-            jump_down=not jump,  # TODO: check if above platform
-            aim=aim,  # TODO: prediction
-            shoot=True,  # TODO: check wall in the front
-            reload=False,  # TODO
+            jump_down=jump_down,
+            aim=aim,
+            shoot=shoot,
+            reload=reload,
             swap_weapon=False,
             plant_mine=False
         )
